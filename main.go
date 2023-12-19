4d2c2e62ddb7507c31ef1fc36c0a4eaa6fe96de2
Oudart Matéo <mateooudart@gmail.com>
2023-12-19 12:12:21 +0100

    added add & remove

--- main.go
+++ main.go
@@ -13,10 +13,20 @@ func main() {
 	fmt.Println(dictio)
 
 	add(dictio, "key3", 13)
+	get(dictio, "key3")
+	remove(dictio, "key3")
 	fmt.Println(dictio)
 
 }
 
+func get(dictionnaire map[string]int, key string) {
+	fmt.Println(dictionnaire[key])
+}
+
 func add(dictionnaire map[string]int, key string, value int) {
 	dictionnaire[key] = value
+}
+
+func remove(dictionnaire map[string]int, key string) {
+	delete(dictionnaire, key)
 }
